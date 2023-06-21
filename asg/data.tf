7e0f357698ddd65faeb7102a233518395d038977
Abbosbek Umarkhodzhaev <128778585+sky000743@users.noreply.github.com>
2023-06-20 20:12:16 -0500

    added asg

---
+++ asg/data.tf
@@ -0,0 +1,29 @@
+   #Create ASG with Remote state and Pull Subnet information
+
+data "terraform_remote_state" "vpc" { 
+  backend = "remote"
+
+  config = {
+    organization = "sky000743"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+# Finds Ubuntu AMI 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
